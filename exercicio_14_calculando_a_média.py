b14e9a2eaeea4e917a3b8dce57939e56040f4cd7
Danilo <danilosoaresmatos@gmail.com>
2025-08-11 14:42:53 -0300

    solução concluída do "Calculando a Média"

---
+++ exercicio_14_calculando_a_média.py
@@ -0,0 +1,4 @@
+def calcular_media(lista):
+    return sum(lista) / len(lista) if lista else 0
+print(calcular_media([987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]))
+
