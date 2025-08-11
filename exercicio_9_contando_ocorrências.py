7de343fe258647c196649e9bff21ed8e45e03cd9
Danilo <danilosoaresmatos@gmail.com>
2025-08-11 14:41:14 -0300

    solução concluída do "Contando Ocorrências"

---
+++ exercicio_9_contando_ocorrências.py
@@ -0,0 +1,4 @@
+def contar_ocorrencias(lista, valor):
+    return lista.count(valor)
+print(contar_ocorrencias([987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9],9))
+
